65f145b5b249660c9cecb676218107663b9f82d4
Bennett <hello@vantezzen.io>
2025-06-24 11:49:34 +0200

    Update rulesets.ts

--- src/lib/config/rulesets.ts
+++ src/lib/config/rulesets.ts
@@ -1,5 +1,7 @@
 import BenchConstraints from "@/assets/constraints/Bench.constraints.json";
 import FiretrucksGlobalConstraints from "@/assets/constraints/Firetrucks.global-constraints.json";
+import UtilityGlobalConstraints from "@/assets/constraints/Utility.global-constraints.json";
+import ZebraCrossingConstraints from "@/assets/constraints/ZebraCrossing.constraints.json";
 import type { ConstraintSet } from "../types/schemas/acs.schema";
 import type { GlobalConstraints } from "../types/schemas/agcs.schema";
 
@@ -7,7 +9,11 @@ function asConstraintSet(json: any): ConstraintSet {
   return json as ConstraintSet;
 }
 export const ObjectRulesets: Record<string, ConstraintSet[]> = {
-  bench: [asConstraintSet(BenchConstraints)],
+  bench: [
+    asConstraintSet(BenchConstraints),
+    // asConstraintSet(PerformanceConstraints),
+  ],
+  zebraCrossing: [asConstraintSet(ZebraCrossingConstraints)],
 };
 
 function asGlobalConstraintSet(json: any): GlobalConstraints {
@@ -15,4 +21,5 @@ function asGlobalConstraintSet(json: any): GlobalConstraints {
 }
 export const GlobalRulesets: Array<GlobalConstraints> = [
   asGlobalConstraintSet(FiretrucksGlobalConstraints),
+  asGlobalConstraintSet(UtilityGlobalConstraints),
 ];
